c51cb2540148e6934db5e840e8885620d364da9f
Frederik Banis <jazzpilz@posteo.net>
2025-06-24 04:32:35 -0400

    update/server

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Flask server configuration script for the emotion detector setup.
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +7,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_index_page():
+    """Render the `index` page.
+    """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def query_emotion_detector():
+def query_emotion_detector() -> str:
+    """Query the `emotion_detector` function from the `EmotionDetection` 
+    module.
+
+    Returns:
+      infostr (str): String with the information returned by 
+      `emotion_detector`.
+    """
     text_to_analyze = request.args.get("textToAnalyze")
     output = emotion_detector(text_to_analyze)
     anger = output["anger"]
@@ -25,9 +36,9 @@ def query_emotion_detector():
     infostr = f"""For the given statement, the system response
     is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear},
     'joy': {joy} and 'sadness': {sadness}. The dominant emotion
-    is {dominant_emotion}.
-    """
+    is {dominant_emotion}."""
     return infostr
     
+
 if __name__ == '__main__':
     app.run()
